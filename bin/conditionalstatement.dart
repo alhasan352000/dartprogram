62cc95934f8599b9706c6cbd898ecf88e1504285
Hasan <projectsdata100@gmail.com>
2023-03-30 03:17:38 +0600

    conditional-statement-practice

---
+++ bin/conditionalstatement.dart
@@ -0,0 +1,13 @@
+void main(){
+  String ok = 'a';
+
+  if(ok == 'ok'){
+    print("ok...");
+  }
+  else if(ok == 'waiting'){
+    print("waiting...");
+  }
+  else{
+    print("not ok...");
+  }
+}
---
+++ bin/switchcasestatement.dart
@@ -0,0 +1,14 @@
+void main(){
+  String a = 'b';
+
+  switch(a){
+    case 'a':
+      print("a");
+      break;
+    case 'b':
+      print("b");
+      break;
+    default:
+      print("wrong...");
+  }
+}
