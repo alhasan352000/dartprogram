6cad4154b59d78e7c3cbd65b5d15109b86dbc170
Hasan <projectsdata100@gmail.com>
2023-04-02 01:02:48 +0600

    list-practice

---
+++ bin/listexample.dart
@@ -0,0 +1,27 @@
+void main(){
+  List<String> list = ['A', 'B', 'C', 'D'];
+  print(list);
+  print(list.length);
+  print(list.reversed);
+
+  list.add('E');
+  print(list);
+  
+  list.addAll(['F', 'G', 'H']);
+  print(list);
+
+  list.replaceRange(0, 2, ['AA', 'BB']);
+  print(list);
+
+  list.remove(list[2]);
+  print(list);
+
+  list.insert(2, 'C');
+  print(list);
+
+  list.insertAll(0, ['AAA', 'BBB']);
+  print(list);
+
+  print(list.join());
+  print(list.join(', '));
+}
